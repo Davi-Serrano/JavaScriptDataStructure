db9dec160153725ab914f2bc204566310a313f59
Davi Serrano <daviscardoso2901@gmail.com>
2022-09-19 23:26:24 -0300

    graphs code wrote

--- Graphs.js
+++ Graphs.js
@@ -1,11 +1,11 @@
 class Graphs{
     constructor(){
-        this.adjascentList = []
+        this.adjacentList = []
     }
 
     addVertex(vertex){
-        if(!this.adjascentList[vertex]){
-            this.adjascentList[vertex] = []
+        if(!this.adjacentList[vertex]){
+            this.adjacentList[vertex] = []
             return true
         }
 
@@ -13,19 +13,32 @@ class Graphs{
     }
 
     addEdge( vertex1, vertex2 ){
-        if(this.adjascentList[vertex1] && this.adjascentList[vertex2]){
-            this.adjascentList[vertex1].push(vertex2)
-            this.adjascentList[vertex2].push(vertex1)
+        if(this.adjacentList[vertex1] && this.adjacentList[vertex2]){
+            this.adjacentList[vertex1].push(vertex2)
+            this.adjacentList[vertex2].push(vertex1)
         }
     }
 
     removeEdge(vertex1, vertex2){
-        if(this.adjascentList[vertex1] && this.adjascentList[vertex2]){
-            this.adjascentList[vertex1] = this.this.adjascentList[vertex1]
+        if(this.adjacentList[vertex1] && this.adjacentList[vertex2]){
+            this.adjacentList[vertex1] = this.this.adjacentList[vertex1]
                 .filter(v => v !== vertex2)
 
-                this.adjascentList[vertex2] = this.this.adjascentList[vertex2]
+                this.adjacentList[vertex2] = this.this.adjacentList[vertex2]
                  .filter(v => v !== vertex1)
         }
     }
+
+    removeVertex(vertex){
+        if(!this.adjacentList[vertex]) return undefined
+
+        while(this.adjacentList[vertex].length){
+            let temp = this.adjacentList[vertez].pop()
+            this.removeEdge(vertex, temp)
+        }
+
+        delete this.adjacentList[vertex]
+        return this
+    }
+
 }
