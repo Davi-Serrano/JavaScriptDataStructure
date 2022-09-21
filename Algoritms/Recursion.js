1354b4814411e0000f56d0181defe6834593df5b
Davi Serrano <daviscardoso2901@gmail.com>
2022-09-21 08:33:02 -0300

    Recursion comentado

--- Algoritms/Recursion.js
+++ Algoritms/Recursion.js
@@ -1,6 +1,11 @@
-//Call Stack
+//Recursion é quando reutilizamos uma funcção, chamamos ela de volta até não precisar mais ser chamada
 
-function funcThre(){
+
+//Call Stack, não é um exemplo de recursion, mas podemos observar um comportamento semelhante, pois primeiro será chamado a função funcOne, mas a primeira ser executada
+//é a função Trhee
+
+
+function funcThree(){
     console.log("Three")
 }
 
@@ -15,7 +20,8 @@ function funcOne(){
 }
 
 
-//Factorial
+////Fatorial é o exemplo mais comum de recursion pois como o exemplo mostra abaixo o resultado de um número fatorial é sempre ele mesmo vezes o seu
+// numero anterior fatorial isso se repetirá até chegar no número 1 que é a base.
 
 //4! = 4 * 3 * 2 *1
 //4! = 4 * 3!
@@ -23,9 +29,10 @@ function funcOne(){
 //              2! = 2 * 1 
 //                       1 = 1 * 1 
 
-
+//Exemplo que retorna o valor fatorial de n fatorial.
 function factorial(n){
+    
     if(n === 1) return 1
-
+    
     return n * factorial(n-1)
 }
