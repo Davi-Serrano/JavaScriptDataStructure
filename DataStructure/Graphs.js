f32d25550bd9ef16181a8a02aee17dba41777c15
Davi Serrano <daviscardoso2901@gmail.com>
2022-09-21 09:06:45 -0300

    Graphs comentado

--- DataStructure/Graphs.js
+++ DataStructure/Graphs.js
@@ -8,7 +8,10 @@ class Graphs{
         this.adjacentList = []
     }
 
-    //Crirá o item a ser conectado no Graph.
+    //Criará o item a ser conectado no Graph.
+    //Ex: adjacentList: [
+    //      A: [ ]
+    //    ]
     addVertex(vertex){
         //Somente criará se o item ainda não existir .
         if(!this.adjacentList[vertex]){
@@ -19,31 +22,50 @@ class Graphs{
         return false
     }
 
+    //Faz a conexões dos itens passados como parâmetros.
+     //Ex: adjacentList: [
+    //      A: [ "B"],
+    //      B  [ "A"],
+    //    ]
     addEdge( vertex1, vertex2 ){
+        //Somente adicionará se os dois itens existirem.
         if(this.adjacentList[vertex1] && this.adjacentList[vertex2]){
             this.adjacentList[vertex1].push(vertex2)
             this.adjacentList[vertex2].push(vertex1)
         }
     }
 
+    //Removerá as conexões entre dois itens.
     removeEdge(vertex1, vertex2){
+        //Somente fará a conexão se os dois itens existirem.
         if(this.adjacentList[vertex1] && this.adjacentList[vertex2]){
+            
+            //Retira a conexão do item1 um excluindo o item2 de suas conexões,mas mantendo todas as outras.            
             this.adjacentList[vertex1] = this.this.adjacentList[vertex1]
-                .filter(v => v !== vertex2)
-
-                this.adjacentList[vertex2] = this.this.adjacentList[vertex2]
-                 .filter(v => v !== vertex1)
+                    .filter(v => v !== vertex2)
+            
+            //Retira a conexão do item2 um excluindo o item1 de suas conexões,mas mantendo todas as outras.            
+            this.adjacentList[vertex2] = this.this.adjacentList[vertex2]
+                    .filter(v => v !== vertex1)
         }
     }
 
+    //Remoce um item do Graph.
     removeVertex(vertex){
+        //Somente removerá o item se ele existir.
         if(!this.adjacentList[vertex]) return undefined
-
+    
+        //Para remover o item primeiro ele removerá todas as suas conexões de entre os outros itens.
+        
+        //Enquanto ainda houver conexnões dentro do item ele removerá o último item, e atribuirá seu valor a TEMP.
+        //Após isso usará o método removeEdge para que posso remover todas as conexões. 
+        
         while(this.adjacentList[vertex].length){
-            let temp = this.adjacentList[vertez].pop()
+            let temp = this.adjacentList[vertex].pop()
             this.removeEdge(vertex, temp)
         }
 
+        //Após isso deletará  o item.
         delete this.adjacentList[vertex]
         return this
     }
