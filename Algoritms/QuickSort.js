b8b2401067b32cda5e2578a9db55d2b896f71878
Davi Serrano <daviscardoso2901@gmail.com>
2022-09-26 11:41:54 -0300

    comentando aqui

--- Algoritms/QuickSort.js
+++ Algoritms/QuickSort.js
@@ -1,3 +1,14 @@
+//Nesse algoritimo ele ordena os itens do array de uma maneira um pouco diferente
+//ele irá analisar o primerio item do array em ralação aos outros e depois colocará os menores à sua esquerda e os maiores à sua direita
+//feito isso ele ordenará os itens da esquerda depois os da direita e fará assim com que o array esteja ordenado.
+
+//Esse algoritimo é divido em três funções:
+
+//Swap - Essa função mudará a posição do primeiro item com o segundo. se quisermos que o maior número fique a direita colocaremos como o no lugar de secondIndex,
+// nosso primerio indice do array como firtsIndex
+//Caso queiramos que o item fique á esquerda passamos ele como o firtsIndex e o primerio item do array como secondIndex
+//O primerio parâmetro sempre será o array que desejamos usar o quickSort
+
 function swap(array, firtsIndex, secondIndex){
     let temp = array[firtsIndex]
     array[firtsIndex] = array[secondIndex]
@@ -5,15 +16,25 @@ function swap(array, firtsIndex, secondIndex){
 
 }
 
+//Pivot -Irá percorrer todo o array e vai analisar qual item é maior que o primerio item do array, se for adicionará +1 ao SwapIndex e colocará o item à direita 
+// caso não seja chamará a função swap novamente que colocará o item à esquerda.
+
 function pivot( array, pivotIndex=0, endIndex=array.length-1){
+    //Primeiro index do array
     let swapIndex = pivotIndex
+    
+    //Percorerrá todo o array menor o último item.
     for(let i = pivotIndex + 1; i <= endIndex; i++){
+        
+        //Se o item posterior for maior que o primerio item do array chamrará a função swap
         if(array[i] < array[pivotIndex]){
+            //Esse código abaixo fará com que o menor item do array fique à esquerda do primeiro indice
             swapIndex++
             swap(array, swapIndex, i)
         }
     }
 
+    //Esse código abaixo fará com que o maior item do array fique à direita do primeiro indice.
     swap(array, pivotIndex, swapIndex)
     return swapIndex
 }
